6cb2434c7cf6b941f6c39327ffb88f92007b35ab
Aashish Dhiman <aashishdhiman88@gmail.com>
2023-08-04 17:26:38 +0530

    implemented login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
-import { hashPassword } from "../helper/authHelper.js";
+import { hashPassword, comparePassword } from "../helper/authHelper.js";
 import userModel from "../models/userModel.js";
+import JWT from "jsonwebtoken";
 
+//POST REGISTER
 export const registerController = async (req, res) => {
     try {
         const { name, email, password, phone, address } = req.body;
@@ -23,7 +25,6 @@ export const registerController = async (req, res) => {
 
         //Register User
         const hashedPassword = await hashPassword(password);
-        console.log("hashed Password:", hashedPassword);
 
         const user = await new userModel({
             name,
@@ -43,6 +44,67 @@ export const registerController = async (req, res) => {
         res.status(500).send({
             success: false,
             message: "Error in Registration",
+            error,
+        });
+    }
+};
+
+//POST LOGIN
+export const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        //Checking the EMAIL and PASSWORD
+        if (!email || !password) {
+            return res.status(401).send({
+                success: false,
+                message: "Invalid username or password",
+            });
+        }
+
+        //FINDING THE USER
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            return res.status(401).send({
+                success: false,
+                message: "User Not Registered!",
+            });
+        }
+
+        //IF USER EXISTS-CHECKING THE PASSWORD
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return res.status(401).send({
+                success: false,
+                message: "Invalid Password!",
+            });
+        }
+
+        //TOKEN
+        const token = await JWT.sign(
+            { _id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        //SUCCESS RESPONSE
+        res.status(200).send({
+            success: true,
+            message: "LoggedIn Successfully!",
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+            },
+            token,
+        });
+    } catch (error) {
+        console.log("Login error: " + error);
+        res.status(500).send({
+            success: false,
+            message: "Error in Login",
             error,
         });
     }
