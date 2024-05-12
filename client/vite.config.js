973fe1a073bb89384453e3eaab9eb46bec15bf35
Aashish Dhiman <aashishdhiman88@gmail.com>
2024-05-12 15:50:11 +0530

    fix proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -10,8 +11,26 @@ export default defineConfig({
             "/api": {
                 target: "https://e-commerce-mgtd.onrender.com", // Replace with your backend server URL
                 changeOrigin: true,
-                secure: true,
-                ws: true,
+                secure: false,
+                configure: (proxy, _options) => {
+                    proxy.on("error", (err, _req, _res) => {
+                        console.log("proxy error", err);
+                    });
+                    proxy.on("proxyReq", (proxyReq, req, _res) => {
+                        console.log(
+                            "Sending Request to the Target:",
+                            req.method,
+                            req.url
+                        );
+                    });
+                    proxy.on("proxyRes", (proxyRes, req, _res) => {
+                        console.log(
+                            "Received Response from the Target:",
+                            proxyRes.statusCode,
+                            req.url
+                        );
+                    });
+                },
             },
         },
     },
